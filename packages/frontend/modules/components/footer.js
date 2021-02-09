0e8364afff5243935533436829fa93519b9c7a99
遠藤雅俊 <>
2021-02-09 17:30:12 +0900

    フッター作成

---
+++ packages/frontend/modules/components/footer.js
@@ -0,0 +1,12 @@
+import styles from '../../styles/modules/components/Footer.module.sass'
+
+const Footer = () => (
+  <footer className={styles.cFooter}>
+    <div className={styles.bg}></div>
+    <div className="sInner">
+      <p className={styles.copyright}>Masa-Endo © 2021</p>
+    </div>
+  </footer>
+)
+
+export default Footer
--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 import Header from '../modules/components/header'
+import Footer from '../modules/components/footer'
 
 export default function Home({ blog }) {
 
@@ -55,6 +56,7 @@ export default function Home({ blog }) {
           </ul>
         </div>
       </section>
+      <Footer/>
     </div>
   );
 }
